5665b2e524005801a1542bbd6082e0c0588d0dc2
Mich DMark <michdmark@gmail.com>
2022-07-20 23:24:07 -0500

    tipos de enteros

--- 02/var_let.playground/Contents.swift
+++ 02/var_let.playground/Contents.swift
@@ -7,3 +7,24 @@ var currentLoginAttempt = 0
 //declarar varias variables juntas
 //ej poner unas coordenadas
 var x = 0.0 , y = 0.0, z = 0.0
+
+//para declarar una variable sin valor inicial, decir tipo de variable
+var welcomeMessage  : String
+
+var count_i : Int
+
+var red, green, blue : Double
+
+
+count_i = 4
+red = 255
+welcomeMessage = "Hola soy Mich DMark"
+
+//se pueden usar simbolos en variables
+//no recomendable
+let π = 3.141592
+
+/* swift tiene autocompletar en los
+ comentarios multilineas*/
+
+print("el numero de login actual es: \(currentLoginAttempt) de un total disponible de: \(maximumNumberOfLoginAttempts)")
---
+++ 03/numbers.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+// cual es el numero minimo y maximo de 8 bits
+// UInt8 es unsigned integer 8
+// enteros sin signo de 8 bits
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
+let maxValue32 = UInt32.max
+
+//declaramos que tipo de entero necesitamos
+//para no desperdiciar memoria
+let age : UInt8 = 31
